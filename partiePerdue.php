d3030ec7bde0c701dc045c1634998300c917cedc
Reflobapi <clayette.pierre@gmail.com>
2015-04-26 17:57:54 +0200

    Game play 1vs1 fonctionnel à 95%
    Desolé j'ai fail, c'est celui ci le bon.
    Ps: Bande de biatch

---
+++ partiePerdue.php
@@ -0,0 +1,23 @@
+<?php
+include("header.php");
+$login_user = $_SESSION['login_user'];
+$id_game = $_GET['id_game'];
+$bdd->query("UPDATE game SET statut_game = 0 WHERE id_game = '$id_game'");
+
+$reponse = $bdd->query("SELECT nbPlayed_user, nbLoose_user FROM user WHERE login_user = '$login_user'");
+
+while($donnees = $reponse->fetch())
+{
+	$nbLoose_joueur = $donnees['nbLoose_user'];
+	$nbPlayed_joueur = $donnees['nbPlayed_user'];
+}
+$nbPlayed_joueur++;
+$nbLoose_joueur++;
+$bdd->query("UPDATE user SET nbPlayed_user = '$nbPlayed_joueur' AND nbLoose_user = '$nbLoose_joueur' WHERE login_user = '$login_user'");
+$bdd->query("UPDATE game SET statut_game = 0 WHERE id_game = '$id_game'");
+?>
+
+<p>Sorry you lost! You should be faster to win!</p>
+
+<a href="ranking.php">Check my ranking</a>
+<a href="accueilPartie.php">Back to main menu</a>
